ebb075cd17c06eb8572b8d203036fb0fd3252869
BakedPotateo <ryanteo2991@gmail.com>
2022-10-06 18:23:29 +0800

    minor formatting

--- Project2/AdjMatrix/Main.java
+++ Project2/AdjMatrix/Main.java
@@ -15,16 +15,7 @@ public class Main {
                                             {3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
                                             {0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0}};
 
-        // for(int i = 0; i < 12; i++)
-        //     for(int j = 0; j <12; j++)
-        //         if (weightedGraph[i][j] != 0)
-        //             weightedGraph[j][i] = weightedGraph[i][j];
-        // int weightedGraph[][] = new int[][]{{0, 2, 1, 0, 0, 0}, 
-        //                                     {2, 0, 7, 0, 8, 4}, 
-        //                                     {1, 7, 0, 7, 0, 3}, 
-        //                                     {0, 0, 7, 0, 8, 4}, 
-        //                                     {0, 8, 0, 8, 0, 5}, 
-        //                                     {0, 4, 3, 4, 5, 0}};
+        
         int source = 0;
         ShortestPath graph = new ShortestPath(12, source);
         graph.Dijkstra(weightedGraph, source);
