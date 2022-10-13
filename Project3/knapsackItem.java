7c55539b573ca1cfbbfb2f24b2c8a4c4e643e14c
BakedPotateo <ryanteo2991@gmail.com>
2022-10-13 09:33:54 +0800

    Created knapsackItem.java

---
+++ Project3/knapsackItem.java
@@ -0,0 +1,25 @@
+package Project3;
+
+public class knapsackItem {
+    private int weight;
+    private int profit;
+    
+    public knapsackItem(){}
+    
+    public knapsackItem(int w, int p){
+        this.weight = w;
+        this.profit = p;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+
+    public int getProfit(){
+        return this.profit;
+    }
+
+    public int getOrder(){
+        return this.profit/this.weight;
+    }
+}
